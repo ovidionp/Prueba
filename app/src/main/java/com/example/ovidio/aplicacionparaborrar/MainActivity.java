26f13f6de2fc736b12ff247a9f2b26a8214b7791
ovidionp <ovidionp10@gmail.com>
2018-02-18 23:24:43 +0100

    hola

--- app/src/main/java/com/example/ovidio/aplicacionparaborrar/MainActivity.java
+++ app/src/main/java/com/example/ovidio/aplicacionparaborrar/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
+    /**hola soy edu feliznavidad
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
